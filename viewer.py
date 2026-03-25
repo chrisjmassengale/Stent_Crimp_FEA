d76cf8ccf5125a344ec3d058ef9f5cce1797dc82
chrisjmassengale <chrisjmassengale@gmail.com>
2026-03-25 00:27:24 -0700

    auto-save

--- viewer.py
+++ viewer.py
@@ -141,6 +141,7 @@ class FramePlayer:
         self._last_key   = None
         self._last_deform_t = 0.
         self.last_tube_tip_z = 0.
+        self._is_axial   = None   # per-vertex axial strut mask (set in _load_anchors)
 
         self._scan()
 
